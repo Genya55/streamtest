46021796ee5c0405dcf15cfa07f0b549fd245c63
Genya55 <nk12mv11033@gmail.com>
2024-04-13 23:04:07 -0500

    mod8

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App({ signOut, user }) {
                 <h2>Hello React with AWS</h2>
                 {user ? (
                     <>
-                        <h3>You have authorityÅF{user.username}</h3>
+                        <h3>You have authority:{user.username}</h3>
                         <button onClick={signOut}>Sign out</button>
                     </>
                 ):(
